c7e698f37dfdd5354ac0c964407344c6c0778b44
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-03 19:50:51 -0700

    updated read me, and indentation

--- client/src/components/SparklingClean.jsx
+++ client/src/components/SparklingClean.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import css from '../../dist/styles.css';
 
-const SparkingClean = () => {
-    return(
-        <div className={css.cleaning}>
-            <div className={css.icon}><i className="fas fa-spray-can fa-xs"></i></div>
-            <div className={css['cleaning-info']}>
-                <div className={css['clean-title']}>Sparkling clean</div>
-                <div className={css['clean-content']}>18 recent guests said this place was sparkling clean.</div>
-            </div>
-        </div>
-    );
-}
+const SparkingClean = () => (
+  <div className={css.cleaning}>
+    <div className={css.icon}><i className="fas fa-spray-can fa-xs" /></div>
+    <div className={css['cleaning-info']}>
+      <div className={css['clean-title']}>Sparkling clean</div>
+      <div className={css['clean-content']}>18 recent guests said this place was sparkling clean.</div>
+    </div>
+  </div>
+);
 
 export default SparkingClean;
--- server/index.js
+++ server/index.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const app = express(); 
-const bodyParser = require('body-parser')
-const db = require('../database/index.js');
+
+const app = express();
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require('../database/index.js');
+
 const port = 3002;
 
 app.use(cors());
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(`${__dirname}/../client/dist`));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/listings/:id', function(req, res) {
-    db.findOne({id: req.params.id}).exec((err, docs) => {
-        if (err) {
-          res.status(500).send();
-        } else {
-          res.status(200).send(docs);
-        }
-      })
-})
-
+app.get('/listings/:id', (req, res) => {
+  db.findOne({ id: req.params.id }).exec((err, docs) => {
+    if (err) {
+      res.status(500).send();
+    } else {console.log(docs);
+      res.status(200).send(docs);
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
