34b5f039daead5f91cb5d6a8bd24995e0c95d197
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-13 01:17:10 -0700

    getListingAsyncParallel

--- postgresDB/controller.js
+++ postgresDB/controller.js
@@ -34,7 +34,6 @@ const getListing = function (targetId, cb) {
   });
 };
 
-
 const getListingAsync = async function (targetId, cb) {
   try {
     const data1 = await pool.query(`SELECT * FROM airbnb.listings WHERE listing_id=${targetId}`);
@@ -44,6 +43,30 @@ const getListingAsync = async function (targetId, cb) {
                                     ON itemgroups.itemgroup_id = items.itemgroup_id  WHERE listing_items.listing_id = ${targetId}`);
     const data3 = await pool.query(`SELECT * FROM airbnb.listing_sleepings WHERE listing_id=${targetId}`);
     
+    const allData = Object.assign({}, data1.rows[0]);
+    allData.items                = data2.rows.length >= 1 ? data2.rows[0] : {};
+    allData.sleepingArrangements = data3.rows.length >= 1 ? data3.rows[0] : {};
+    cb(undefined, allData);
+  } catch (e) {
+    console.log(e);
+    cb(e);
+  }
+};
+
+const getListingAsyncParallel = async function (targetId, cb) {
+  try {
+    const query1 = pool.query(`SELECT * FROM airbnb.listings WHERE listing_id=${targetId}`);
+    const query2 = pool.query(`SELECT listing_id, item_name, itemgroup_name
+                                    FROM (airbnb.listing_items JOIN airbnb.items
+                                    ON listing_items.item_id = items.item_id) JOIN airbnb.itemgroups
+                                    ON itemgroups.itemgroup_id = items.itemgroup_id  WHERE listing_items.listing_id = ${targetId}`);
+    const query3 = pool.query(`SELECT * FROM airbnb.listing_sleepings WHERE listing_id=${targetId}`);
+
+    const data1 = await query1;
+    const data2 = await query2;
+    const data3 = await query3;
+
+
     const allData = Object.assign({}, data1.rows[0]);
     allData.items                = data2.rows.length >= 1 ? data2.rows[0] : {};
     allData.sleepingArrangements = data3.rows.length >= 1 ? data3.rows[0] : {};
@@ -86,6 +109,7 @@ module.exports = {
   getTime,
   getListing,
   getListingAsync,
+  getListingAsyncParallel,
   deleteListing,
   insertListing,
   updateListing,
